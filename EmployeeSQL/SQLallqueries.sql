f2934971c6cb6f3b685c71ced7642bc70d4b75ef
Arti B <artib.9803@gmail.com>
2024-01-14 19:53:30 -0500

    push

---
+++ EmployeeSQL/SQLallqueries.sql
@@ -0,0 +1,38 @@
+--Question 1 (list of employees)
+Select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+from public."Employees" as e
+Join public."Salaries" as s
+on e.emp_no = s.emp_no
+
+--Question 2(employees hired from 1986)
+Select e.first_name, e.last_name, e.hire_date
+from public."Employees" as e
+WHERE hire_date BETWEEN ('1986-01-01') AND ('1986-12-31')
+
+-- Question 3 (manager of each department)
+Select dm.dept_no, d.dept_name, e.emp_no, e.first_name, e.last_name
+from "Dept_manager" as dm
+Inner join "Departments" as d
+on dm.dept_no = d.dept_no
+Inner join "Employees" as e
+on dm.emp_no = e.emp_no
+
+--question 4 (department of each employee)
+select de.dept_no, e.emp_no, d.dept_name, e.first_name, e.last_name
+from "Dept_emp" as de
+inner join "Departments" as d
+on d.dept_no = de.dept_no
+inner join "Employees" as e
+on e.emp_no = de.emp_no
+
+--question 5 (Lastname starts with "b" and first name is Hercules)
+Select e.first_name, e.last_name, e.sex
+from "Employees" as e
+where first_name like 'Hercules' 
+and last_name like '%B%'
+
+--question 6 (employees in the sales department)
+Select d.dept_name, e.emp_no, e.last_name, e.first_name
+from "Departments" as d
+inner join "Employees" as e
+where dept_name like 'Sales'
---
+++ SQLassignmentcsvs.sql
@@ -0,0 +1,61 @@
+CREATE TABLE "Departments" (
+    "dept_no" text   NOT NULL,
+    "dept_name" text   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "Dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" text   NOT NULL
+);
+
+CREATE TABLE "Dept_manager" (
+    "dept_no" text   NOT NULL,
+    "emp_no" int   NOT NULL
+);
+
+CREATE TABLE "Employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" text   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" text   NOT NULL,
+    "last_name" text   NOT NULL,
+    "sex" text   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL
+);
+
+CREATE TABLE "Titles" (
+    "title_id" text   NOT NULL,
+    "title" text   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "Titles" ("title_id");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
